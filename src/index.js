77061f44bbc20e6326e456b1b6f4af8e30a60a92
Mattias Welamsson <mattias@iMac.local>
2022-04-26 11:49:16 +0900

    completed

--- src/index.js
+++ src/index.js
@@ -1,33 +1,30 @@
+
 import React from "react";
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 
-ReactDOM.render(
-  <div>
-    <h1>My Contacts</h1>
+const container = document.getElementById("root");
+const root = createRoot(container);
 
-    <h2>Beyonce</h2>
+root.render(<div>
+  <Card
+   name="YouWish" 
+   img="https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://arweave.net/k8cqGP76VOyFi1lys7mw9RAQTxFNlMwVutHUp7K6j6k?ext=png"
+    tel="123" email="femnant@mail.com"/>
+  <Card name="Femnant1" img="https://arweave.net/KtF7ZUw8tIPKJCCTsvKL-KqGEPx5WDruf1tjK20qT4E?ext=png" tel="123 432 321" email="richassmf@mail.com"/>
+  <Card name="Femnant2" img="https://img-cdn.magiceden.dev/rs:fill:640:640:0:0/plain/https://arweave.net/kqx8Bnvp9XpnmWe5-eTPLgjfKDXbYu4mxRALQvS2Se0?ext=png" tel="321" email="femnant@mail.com"/>
+  </div>)
+
+function Card(props) {
+  return <div>
+     <h2>{props.name}</h2>
     <img
-      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      src={props.img}
       alt="avatar_img"
     />
-    <p>+123 456 789</p>
-    <p>b@beyonce.com</p>
+    <p>{props.tel}</p>
+    <p>{props.email}</p>
 
-    <h2>Jack Bauer</h2>
-    <img
-      src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      alt="avatar_img"
-    />
-    <p>+987 654 321</p>
-    <p>jack@nowhere.com</p>
 
-    <h2>Chuck Norris</h2>
-    <img
-      src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-      alt="avatar_img"
-    />
-    <p>+918 372 574</p>
-    <p>gmail@chucknorris.com</p>
-  </div>,
-  document.getElementById("root")
-);
+  </div>
+};
+
